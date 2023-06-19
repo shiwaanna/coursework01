ce477e28989d14e6c35ce2cbe0e4465c26e1651d
anna <shiwa.ann@gmail.com>
2023-06-19 22:19:03 +0400

    Курсовая базовая сложность

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Storage storage = new Storage();
+        storage.printAllEmployees();
+        System.out.println("Сумма затрат на зарплаты в месяц: " + storage.calculateAllSalaries());
+        System.out.println("Сотрудник с минимальной зарплатой " + storage.findPoorEmployee());
+        System.out.println("Сотрудник с максимальной зарплатой " + storage.findRichEmployee());
+        System.out.println("Среднее значение зарплат: " + storage.calculateAverageSalary());
+        storage.printFullName();
     }
 }
